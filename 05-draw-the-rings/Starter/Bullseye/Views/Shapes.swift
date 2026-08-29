afd02699a53d9df6540377a53d0522b0837f1fdb
Franklin Byaruhanga <byaruhaf@protonmail.com>
2026-08-29 17:29:47 +0300

    Update for Module 2

---
+++ 05-draw-the-rings/Starter/Bullseye/Views/Shapes.swift
@@ -0,0 +1,25 @@
+import SwiftUI
+
+struct Shapes: View {
+  var body: some View {
+    VStack {
+      Circle()
+        .strokeBorder(Color.blue, lineWidth: 20)
+        .frame(width: 200, height: 100)
+        .background(Color.green)
+      RoundedRectangle(cornerRadius: 20)
+        .fill(Color.blue)
+        .frame(width: 200, height: 100)
+      Capsule()
+        .fill(Color.blue)
+        .frame(width: 200, height: 100)
+      Ellipse()
+        .fill(Color.blue)
+        .frame(width: 200, height: 100)
+    }
+  }
+}
+
+#Preview {
+    Shapes()
+}
